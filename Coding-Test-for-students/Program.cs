4f6c60872e5f3bce11ad8f113962f52b83160272
William Cram <FDNeo_83@hotmail.com>
2017-07-16 21:11:32 -0700

    Solution Build

---
+++ Coding-Test-for-students/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Coding_Test_for_students
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("7 months later, it has started. - BC");
+
+            Console.ReadKey();
+
+        }
+    }
+}
